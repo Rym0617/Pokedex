0dccfad7a408855b454345a92f20b72065417fd4
rym0617 <brenovales1@gmail.com>
2024-09-20 18:35:25 -0400

    Pkdx_240920_CreatePokemon

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,7 +11,7 @@ import { PokemonModule } from './pokemon/pokemon.module';
       rootPath: join(__dirname,'..','public'),
     }),
 
-    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+    MongooseModule.forRoot('mongodb://127.0.0.1:27017/nest-pokemon'),
 
     PokemonModule
   ],
